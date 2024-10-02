b535d15074e4ce5b973d01b4ccb9e4270179959e
S4BS0N <57192510+S4BS0N@users.noreply.github.com>
2024-10-02 20:59:22 +0400

    Add files via upload

---
+++ print (hello world).py
@@ -0,0 +1,40 @@
+
+def myfunc():
+    
+    x = " magari"
+
+    print("vin aris" + x)
+    print("lansolo") + print("vinsolo")
+
+myfunc()
+
+#print (x)
+
+
+
+username = input("User: ")
+
+
+if username == "Admin":
+    print("Hi N3O")
+else:
+    print ("Fuck OFF")
+
+
+import sys
+
+print ("Hi")
+
+age = int(input("Ramdeni Wlis Xar? "))
+
+height = float(input("Sheni Simagle? "))
+
+
+with open("user_data.txt", "a") as file:
+    
+
+
+
+
+    file.write(f"User Age: {age}\n")
+    file.write(f"User Height {height}\n")
